aa6bcc72590b1b06f4cf15c70b4988b824764887
wenhaiqing <243083741@qq.com>
2017-10-23 15:25:32 +0800

    后台展示技师和需求详情

--- app/Services/Admin/DemandService.php
+++ app/Services/Admin/DemandService.php
@@ -207,11 +207,8 @@ Eof
 	public function show($id)
 	{
 		try {
-			$user = UserRepositoryEloquent::with(['userPermissions', 'roles'])->find(decodeId($id, $this->module));
-			$permissions = $this->getAllPermissions();
-			$roles = $this->getAllRole();
-			dump($user->toArray());dump($permissions);dd($roles->toArray());
-			return compact('user', 'permissions', 'roles');
+			$demand = DemandRepositoryEloquent::with(['user', 'cate'])->find(decodeId($id, $this->module));
+			return compact('demand');
 		} catch (Exception $e) {
 			flash(trans('common.find_error'), 'danger');
 			return redirect()->route($this->indexRoute);
