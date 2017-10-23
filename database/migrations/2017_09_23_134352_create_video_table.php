5bda3c627e2bd60e213b185597f265b6cc269af7
wenhaiqing <243083741@qq.com>
2017-10-23 17:23:34 +0800

    后台展示技师和需求详情

---
+++ database/migrations/2017_09_23_134352_create_video_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateVideoTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('videos', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('sid')->unsigned()->index()->comment('技师ID');
+            $table->string('path')->default('')->comment('视频路径')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('videos');
+    }
+}
